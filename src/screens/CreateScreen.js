3378f69a84119e6f9d3e8ecdc5df181c28fa2c7f
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-06 22:20:42 -0800

    add local state for inputs.

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,49 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
-const CreateScreen = () => {
+const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
-    <View>
-      <Text>This is the CreateScreen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={text => setTitle(text)}
+        style={styles.inputStyle}
+      />
+      <Text style={styles.label}>Enter Contents:</Text>
+      <TextInput
+        value={content}
+        onChangeText={text => setTitle(text)}
+        style={styles.inputStyle}
+      />
+      <Button title="Add Blog Post" />
     </View>
   );
 };
 
-styles = StyleSheet.create({});
+styles = StyleSheet.create({
+  viewStyle: {
+    alignItems: "center"
+  },
+  inputStyle: {
+    fontSize: 18,
+    width: 300,
+    height: 50,
+    borderWidth: 2,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    marginTop: 20,
+    fontSize: 20,
+    marginBottom: 5
+  }
+});
 
 export default CreateScreen;
