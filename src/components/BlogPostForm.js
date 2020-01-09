55907ed2c7bd3a4b2f50667ce15ff5682dd8cc95
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-08 17:53:28 -0800

    extract form logic.

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const BlogPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={text => setTitle(text)}
+        style={styles.inputStyle}
+      />
+      <Text style={styles.label}>Enter Contents:</Text>
+      <TextInput
+        value={content}
+        onChangeText={text => setContent(text)}
+        style={styles.inputStyle}
+      />
+      <Button onPress={() => {}} title="Save Blog Post" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    alignItems: "center"
+  },
+  inputStyle: {
+    fontSize: 18,
+    width: 300,
+    height: 50,
+    borderWidth: 2,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    marginTop: 20,
+    fontSize: 20,
+    marginBottom: 5
+  }
+});
+
+export default BlogPostForm;
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,57 +1,14 @@
-import React, { useContext, useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
   const { addBlogPost } = useContext(Context);
 
-  return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput
-        value={title}
-        onChangeText={text => setTitle(text)}
-        style={styles.inputStyle}
-      />
-      <Text style={styles.label}>Enter Contents:</Text>
-      <TextInput
-        value={content}
-        onChangeText={text => setContent(text)}
-        style={styles.inputStyle}
-      />
-      <Button
-        onPress={() =>
-          addBlogPost(title, content, () => {
-            navigation.navigate("Index");
-          })
-        }
-        title="Add Blog Post"
-      />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
-styles = StyleSheet.create({
-  viewStyle: {
-    alignItems: "center"
-  },
-  inputStyle: {
-    fontSize: 18,
-    width: 300,
-    height: 50,
-    borderWidth: 2,
-    borderColor: "black",
-    marginBottom: 15,
-    padding: 5,
-    margin: 5
-  },
-  label: {
-    marginTop: 20,
-    fontSize: 20,
-    marginBottom: 5
-  }
-});
+styles = StyleSheet.create({});
 
 export default CreateScreen;
