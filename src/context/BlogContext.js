ecb4d3a7e50f92d7ddc848963dc5f93c85ceb737
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-09 22:10:01 -0800

    "add reducer for editScreen."

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,6 +13,10 @@ const blogReducer = (state, action) => {
           content: action.payload.content
         }
       ];
+    case "edit_blogpost":
+      return state.map(blogPost => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     default:
       return state;
   }
@@ -31,8 +35,14 @@ const deleteBlogPost = dispatch => {
   };
 };
 
+const editBlogPost = dispatch => {
+  return (id, title, content) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: "kjkldsjdkj", content: "fkjfkjfksj", id: 1 }]
 );
